b0d337b62625ea318d339060959ef3b7fcbc54b7
Jonathan Martin <60260218+jonamartin@users.noreply.github.com>
2020-04-14 23:36:51 -0300

    Add files via upload
    Solución propuesta ejercicio
    Escribir un programa que pregunte el nombre del usuario en la consola y un número entero e imprima por pantalla en líneas distintas el nombre del usuario tantas veces como el número introducido.

---
+++ Repeticion.py
@@ -0,0 +1,11 @@
+print("------------------------------------")
+nombre = input("Ingresa tu nombre: ")
+print("------------------------------------")
+veces = int(input("¿cuantas veces lo repito? Ingresa un numero: "))
+print("------------------------------------")
+a = 1
+while a <= veces:
+    print("------------------------------------")
+    print(nombre)
+    print("------------------------------------")
+    a = a+1
